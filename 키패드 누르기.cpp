633b109bdaa4349fa57aec01236afdac0bff796e
whipbaek <jibaek0513@gmail.com>
2022-01-17 20:36:17 +0900

    level1 kakao2020

--- 키패드 누르기.cpp
+++ 키패드 누르기.cpp
@@ -1,33 +1,16 @@
-#include <bits/stdc++.h>
-
+#include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-/*
-	처음 왼손 엄지는 '*'
-	처음 오른 엄지는 '#'
-
-	1. 엄지는 상하좌우 4가지 방향으로 이동 가능, 이동 한칸은 거리 1에 해당
-	2. 왼쪽 열의 1, 4, 7은 엄지 왼손
-	3. 오른 열의 3, 6, 9는 오른 엄지
-	4. 가운데의 2, 5, 8, 0은 두 엄지중 더 가까운곳
-		4-1. 거리가 같다면 왼손잡이냐, 오른손잡이냐에 따라 판단한다.
-
-	numbers: 순서대로 누를 번호가 담긴 배열
-	hand: 왼손잡이, 오른손잡이
-
-	*/
-
-int main(void) {
-
-	vector<int> numbers;
-	string hand;
+string solution(vector<int> numbers, string hand) {
 	string answer = "";
 
-	int nowLeft;
-	int nowRight;
+	int nowLeft = 10;
+	int nowRight = 11;
 
-	int dist[10][10];
-	fill(&dist[0][0], &dist[9][10],-1);
+	int dist[12][12]; // '*' : 10, '#' : 11
+	fill(&dist[0][0], &dist[9][10], -1);
 
 	//from Left to Center
 	dist[1][2] = 1;
@@ -44,6 +27,11 @@ int main(void) {
 	dist[7][5] = 2;
 	dist[7][8] = 1;
 	dist[7][0] = 2;
+
+	dist[10][2] = 4;
+	dist[10][5] = 3;
+	dist[10][8] = 2;
+	dist[10][0] = 1;
 
 	//from Right to Center
 
@@ -62,6 +50,33 @@ int main(void) {
 	dist[9][8] = 1;
 	dist[9][0] = 2;
 
+	dist[11][2] = 4;
+	dist[11][5] = 3;
+	dist[11][8] = 2;
+	dist[11][0] = 1;
+
+	//from Center to Center
+
+	dist[2][2] = 0;
+	dist[2][5] = 1;
+	dist[2][8] = 2;
+	dist[2][0] = 3;
+
+	dist[5][2] = 1;
+	dist[5][5] = 0;
+	dist[5][8] = 1;
+	dist[5][0] = 2;
+
+	dist[8][2] = 2;
+	dist[8][5] = 1;
+	dist[8][8] = 0;
+	dist[8][0] = 1;
+
+	dist[0][2] = 3;
+	dist[0][5] = 2;
+	dist[0][8] = 1;
+	dist[0][0] = 0;
+
 	//process
 
 	for (int i = 0; i < numbers.size(); i++) {
@@ -77,12 +92,29 @@ int main(void) {
 			nowRight = num;
 		}
 
-		else { 
-			if(dist[])
+		else { //2, 5, 8, 0 에 대한 처리
+			if (dist[nowLeft][num] < dist[nowRight][num]) { //왼쪽 엄지가 더 가깝다면
+				nowLeft = num;
+				answer.push_back('L');
+			}
+
+			else if (dist[nowLeft][num] > dist[nowRight][num]) { //오른 엄지가 더 가깝다면
+				nowRight = num;
+				answer.push_back('R');
+			}
+
+			else { //거리가 같다면
+				if (hand == "left") {
+					nowLeft = num;
+					answer.push_back('L');
+				}
+				else {
+					nowRight = num;
+					answer.push_back('R');
+				}
+			}
 		}
-
 	}
-	*/
 
-	return 0;
+	return answer;
 }
